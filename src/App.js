37cf0da5f9150dbbc270357c04d7dc5c91afb7a5
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-11-19 00:16:22 +0530

    NavBar Part Done

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import NavBar from "./Components/NavBar";
-import {
-  CustomerReviewSection,
-  Footer,
-  Hero,
-  PopularProduct,
-  Services,
-  SpecialOffers,
-  Subcribe,
-  SuperQuality,
-} from "./Sections/SectionController";
+import Main from "./Main";
 
 function App() {
   return (
-    <main className="relative">
-      <NavBar/>
-      <section className="xl:padding-1 wide:padding-r padding-b">
-        <Hero />
-      </section>
-      <section className="padding">
-        <PopularProduct />
-      </section>
-      <section className="padding">
-        <SuperQuality />
-      </section>
-      <section className="padding-x py-10">
-        <Services />
-      </section>
-      <section className="padding">
-        <SpecialOffers />
-      </section>
-      <section className="bg-pale-blue padding">
-        <CustomerReviewSection />
-      </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
-        <Subcribe />
-      </section>
-      <section className="bg-black padding-x padding-t pb-8">
-        <Footer />
-      </section>
-    </main>
+    <>
+    <Main/>
+    </>
   );
 }
 
