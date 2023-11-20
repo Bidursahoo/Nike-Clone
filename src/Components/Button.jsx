08aa8f4f3d6650bb05d40d0fcd9567b7402c0fc2
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-11-20 22:35:14 +0530

    added service special offers and superquality part

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 export default function Button(props) {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+    
+    ${props.backgroundColour ? `${props.backgroundColour} ${props.textColour} ${props.borderColour}` : 'bg-coral-red  text-white border-coral-red' }
+    
+    `}>
         {props.label}
-        <img src={props.iconUrl} alt={props.label} className='ml-2 rounded-full w-5 h-5'/>
+        {
+          props.iconUrl && <img src={props.iconUrl} alt={props.label} className='ml-2 rounded-full w-5 h-5'/>
+        }
     </button>
   )
 }
--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { services } from '../Constants'
+import ServiceCard from '../Components/ServiceCard'
 
 export default function Services() {
   return (
-    <div>Services</div>
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+        {services.map((service)=>(
+          < ServiceCard key={service.label} {...service}/>
+        ))}
+
+    </section>
   )
 }
