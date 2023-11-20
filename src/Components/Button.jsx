b9884796500f5a067b77451d48b7fbd724fda91b
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-11-20 23:28:28 +0530

    Subcribe section Done

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Button(props) {
   return (
-    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${props.fullWidth && 'w-full'}
     
     ${props.backgroundColour ? `${props.backgroundColour} ${props.textColour} ${props.borderColour}` : 'bg-coral-red  text-white border-coral-red' }
     
